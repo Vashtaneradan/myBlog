37bf93e9a78194cf67a421a574473845e42b049e
Chris Bader <chris.bader@schnapptack.de>
2017-05-05 17:08:34 +0200

    started project, started app

---
+++ myBlog/blog_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Post(models.Model):
+    publish_date = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+
+class Author(models.Model):
+    usermame = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
