cb513844e9677c9c8ba5135e3a200f17e7a0c2d7
Chris Bader <chris.bader@schnapptack.de>
2017-05-22 17:17:13 +0200

    added detailview for posts, added to readmore return and updatefunctionatity

--- myBlog/blog_app/urls.py
+++ myBlog/blog_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^post_detail/(?P<pk>[0-9]+)$', views.single_post, name='single_post'),
+    url(r'^post_detail/(?P<pk>[0-9]+)/update$', views.PostUpdate.as_view(), name='update')
 ]
--- myBlog/blog_app/views.py
+++ myBlog/blog_app/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
 
 from django.http import HttpResponse
 
 
 def index(request):
     all_posts = Post.objects.all()
+    print(all_posts)
     return render(request, 'index.html', {'all_posts': all_posts})
 
 
+def single_post(request, pk):
+    post_detail = Post.objects.get(id=pk)
+    print('single_post', post_detail)
+    return render(request, 'post_detail.html', {'post_detail': post_detail})
+
+
+class PostUpdate(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('index')
